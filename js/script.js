b03a25c96d30d6697e95c1dfb1d6f831cf582080
Juliana Rodrigues Nogueira <julianarnogueira28@hotmail.com>
2023-02-20 16:50:38 -0300

    adicionado script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+class Produto {
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = "";
+        this.valor = 0;
+
+    }
+    adicionar() {
+        alert('Adicionou um produto!');
+    }
+    remover(){
+        alert('Removeu produto!')
+    }
+}
+let produto = new Produto ();
